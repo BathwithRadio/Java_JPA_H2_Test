26ace6148f9a1178d758396ec14c3fc61b76e1eb
유민기 <mgyou@nextree.io>
2021-12-22 20:09:57 +0900

    12.14 공부

--- chapter02_03/src/main/java/start/JpaMain.java
+++ chapter02_03/src/main/java/start/JpaMain.java
@@ -7,7 +7,7 @@ import java.util.List;
  * @author holyeye
  */
 public class JpaMain {
-
+    //
     public static void main(String[] args) {
 
         //엔티티 매니저 팩토리 생성
@@ -19,7 +19,9 @@ public class JpaMain {
         try {
 
             tx.begin(); //트랜잭션 시작
+            memberInsert(em); //데이터 입력
             logic(em);  //비즈니스 로직
+            memberDelete(em); //데이터 1건 삭제 후 조회
             tx.commit();//트랜잭션 커밋
 
         } catch (Exception e) {
@@ -33,27 +35,51 @@ public class JpaMain {
     }
 
     public static void logic(EntityManager em) {
-
-        Member member = new Member();
-        member.setId("member1");
-        member.setUsername("회원1");
-
-        //등록
-        em.persist(member);
-
         //한 건 조회
-        Member findMember = em.find(Member.class, "member1");
+        Member findMember = em.find(Member.class, "memberA");
         System.out.println("findMember=" + findMember.getUsername());
 
         //목록 조회
         List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+        for (Member member : members){
+            System.out.println("member name ::: " + member.getUsername());
+        }
         System.out.println("members.size=" + members.size());
 
-        Member a = em.find(Member.class, "member1");
-        Member b = em.find(Member.class, "member1");
+        //비교
+//        Member a = em.find(Member.class, "member1");
+//        Member b = em.find(Member.class, "member1");
+//
+//        System.out.print("a and b are same :: ");
+//        System.out.println(a == b);
 
-        System.out.print("a and b are same :: ");
-        System.out.println(a == b);
+    }
 
+    public static void memberInsert(EntityManager em ){
+        //
+        Member memberA = new Member();
+        memberA.setId("memberA");
+        memberA.setUsername("회원A");
+        memberA.setAge(21);
+
+        Member memberB = new Member();
+        memberB.setId("memberB");
+        memberB.setUsername("회원B");
+        memberB.setAge(322);
+
+        em.persist(memberA);
+        em.persist(memberB);
+    }
+
+    public static void memberDelete(EntityManager em){
+        //
+        //삭제
+        Member memberA = em.find(Member.class, "memberA");
+        em.remove(memberA);
+        //삭제 후 조회
+        List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+        for (Member member : members){
+            System.out.println("member name after deleted::: " + member.getUsername());
+        }
     }
 }
