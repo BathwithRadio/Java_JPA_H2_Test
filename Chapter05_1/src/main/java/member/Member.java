ef8044209658c9b184c28221a58ab7111a61acbe
유민기 <mgyou@nextree.io>
2021-12-27 09:01:40 +0900

    5.6.3 연관관계 편의 메소드 주의사항

--- Chapter05_1/src/main/java/member/Member.java
+++ Chapter05_1/src/main/java/member/Member.java
@@ -35,6 +35,10 @@ public class Member {
     //setter수정
     public void setTeam(Team team) {
         //
+        //기존 관계 제거
+        if (this.team != null) {
+            this.team.getMembers().remove(this);
+        }
         this.team = team;
         team.getMembers().add(this);
     }
